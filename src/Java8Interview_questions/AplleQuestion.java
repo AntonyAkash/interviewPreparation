de65a38e44858d8ae6b346de1dd90b0732493bac
AntonyAkash <antoney97@gmail.com>
2026-03-24 14:10:43 +0530

    Add Altimetrix class for array manipulation and target sum functionality

--- src/Java8Interview_questions/AplleQuestion.java
+++ src/Java8Interview_questions/AplleQuestion.java
@@ -1,5 +1,7 @@
 package Java8Interview_questions;
 
+import java.util.Arrays;
+
 public class AplleQuestion {
 	public static void main(String[] args) {
         int arr[] = {8, 9, 5, 11, 6, 1, 7, 6};
@@ -19,5 +21,49 @@ public class AplleQuestion {
         for (int i : arr) {
             System.out.println(i);
         }
+    }
+
+    public static class Altimetrix {
+        public static void main(String[] args) {
+
+
+            /*
+             * Arrays.stream("Programming".toLowerCase().split("")).collect(Collectors.
+             * groupingBy(Function.identity(),Collectors.counting()))
+             * .entrySet().stream().filter(i->i.getValue()>1).forEach(System.out::println);
+             */
+
+            /*
+             * Given an array of integers nums and an integer target, return indices of the
+             * two numbers such that they add up to target. You may assume that each input
+             * would have exactly one solution, and you may not use the same element twice.
+             * You can return the answer in any order.
+             *
+             * Input: nums = [2,7,11,15], target = 9 Output: [0,1]
+             *
+             * Input: nums = [3,2,4], target = 6 Output: [1,2]
+             */
+
+            int[] arr= {5,7,3,2};
+            var a= Arrays.stream(arr).boxed().filter(i->i%2==0).toList();
+            int target=9;
+            int[] matchingIndexes=findIndexThatSumsToTarget(arr,target);
+            System.out.println(Arrays.toString(matchingIndexes));
+
+
+        }
+
+        private static int[] findIndexThatSumsToTarget(int[] arr,int target) {
+            for(int i=0;i<arr.length-1;i++) {
+                for(int j=i+1;j<arr.length;j++) {
+                    if(arr[i]+arr[j]==target) {
+                        return new int[]{i,j};
+                    }
+                }
+
+            }
+            return new int[] {0};
+        }
+
     }
 }
